9b08fe2f080816212962f11c00676ebc43e822e9
Hardy Images <hardyimages@gmail.com>
2012-06-10 09:19:11 -0700

    na

--- src/ingest.sql
+++ src/ingest.sql
@@ -30,17 +30,16 @@ SET         geo_point = ST_MakePoint(x, y)
 -- create a table with the line segments for all contributions
 DROP TABLE IF EXISTS contrib_paths;
 CREATE TABLE contrib_paths AS
-SELECT      t.lang,
-            t1.node_id AS node_id_src,
+SELECT      t1.node_id AS node_id_src,
             t2.node_id AS node_id_dst
 FROM        contrib_by_month t
-JOIN        contrib_nodes t1 ON (t1.x = t.contrib_x AND t1.y = t.contrib_y AND t1.lang = t.lang)
-JOIN        contrib_nodes t2 ON (t2.x = t.article_x AND t2.y = t.article_y AND t2.lang = t.lang)
-GROUP BY    t.lang, t1.node_id, t2.node_id
+JOIN        contrib_nodes t1 ON (t1.x = t.contrib_x AND t1.y = t.contrib_y)
+JOIN        contrib_nodes t2 ON (t2.x = t.article_x AND t2.y = t.article_y)
+GROUP BY    t1.node_id, t2.node_id
 ;
 
 ALTER TABLE contrib_paths ADD COLUMN path_id serial NOT NULL PRIMARY KEY;
-ALTER TABLE contrib_paths ADD CONSTRAINT ux_contrib_paths UNIQUE(lang, node_id_src, node_id_dst);
+ALTER TABLE contrib_paths ADD CONSTRAINT ux_contrib_paths UNIQUE(node_id_src, node_id_dst);
 ALTER TABLE contrib_paths ADD COLUMN geo_linestring GEOGRAPHY(LINESTRING, 4326);
 ALTER TABLE contrib_paths ADD COLUMN distance_km DOUBLE PRECISION;
 
@@ -49,8 +48,8 @@ select count(*) from contrib_paths;
 UPDATE    contrib_paths p
 SET       geo_linestring = ST_MakeLine(n1.geo_point::geometry, n2.geo_point::geometry)
 FROM      contrib_nodes n1, contrib_nodes n2
-WHERE     n1.lang = p.lang AND n1.node_id = p.node_id_src AND
-          n2.lang = p.lang AND n2.node_id = p.node_id_dst
+WHERE     n1.node_id = p.node_id_src AND
+          n2.node_id = p.node_id_dst
 ;
 
 UPDATE    contrib_paths p
