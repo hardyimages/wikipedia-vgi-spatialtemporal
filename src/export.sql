58325b28ba66a7b5a82eda3bcde596059ce42985
Hardy Images <hardyimages@gmail.com>
2012-06-10 09:29:51 -0700

    na

--- src/export.sql
+++ src/export.sql
@@ -6,7 +6,7 @@ COPY (SELECT * FROM contrib_nodes) TO STDOUT WITH CSV HEADER
 ;
 
 \o ../data/x_contrib_paths.csv
-COPY (SELECT I FROM contrib_paths) TO STDOUT WITH CSV HEADER
+COPY (SELECT * FROM contrib_paths) TO STDOUT WITH CSV HEADER
 ;
 
 -- \o ../data/x_contrib_summary_yyyymm.csv
