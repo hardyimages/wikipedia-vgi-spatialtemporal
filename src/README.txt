b7b8a037687a3dbae316b010410d9dbb010c9da4
Hardy Images <hardyimages@gmail.com>
2012-06-10 07:45:22 -0700

    teats

---
+++ src/README.txt
@@ -0,0 +1,3 @@
+Lang, Year/Month, N edit between x,y -> x,y 
+
+Lang, set of all x,y and N contribs (records)
---
+++ src/ingest.R
@@ -0,0 +1,10 @@
+gc()
+
+k <- "x_contrib_by_month"
+bin_fn <- file.path('..', 'data', paste(k, "RData", sep = "."))
+load(bin_fn)
+for (l in unique(x_contrib_by_month$lang)) {
+  cat('processing', l, '...\n')
+  d <- subset(x_contrib_by_month, lang == l)
+  print(summary(d))
+}
