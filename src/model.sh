85c83f8e8e531ee607d22b0a4718d5cf3c3fd8de
Hardy Images <hardyimages@gmail.com>
2012-06-10 22:19:59 -0700

    x

--- src/model.sh
+++ src/model.sh
@@ -1,13 +1,13 @@
-export PGDATABASE=hardy_dev
-
-echo '\copy (select distinct yyyy, mm, lang from contrib_by_month order by lang, yyyy, mm) to stdout with csv header' | psql -Xq | while read x; do
-    y=`echo $x | cut -d, -f1`
-    m=`echo $x | cut -d, -f2`
-    l=`echo $x | cut -d, -f3`
-    echo running $y $m $l
-    psql -Xq << EOM
-\! test -d ../data/${l} || mkdir ../data/${l}
-\o ../data/${l}/contrib_by_month_${l}_${y}_${m}.csv
-copy (select * from contrib_by_month where yyyy = $y and mm = $m and lang = '$l') to stdout with csv header
-EOM
-done
+# export PGDATABASE=hardy_dev
+# 
+# echo '\copy (select distinct yyyy, mm, lang from contrib_by_month order by lang, yyyy, mm) to stdout with csv header' | psql -Xq | while read x; do
+#     y=`echo $x | cut -d, -f1`
+#     m=`echo $x | cut -d, -f2`
+#     l=`echo $x | cut -d, -f3`
+#     echo running $y $m $l
+#     psql -Xq << EOM
+# \! test -d ../data/${l} || mkdir ../data/${l}
+# \o ../data/${l}/contrib_by_month_${l}_${y}_${m}.csv
+# copy (select * from contrib_by_month where yyyy = $y and mm = $m and lang = '$l') to stdout with csv header
+# EOM
+# done
