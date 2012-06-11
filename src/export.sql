385977fe92c11f261a4714d8503995ec307357d9
Hardy Images <hardyimages@gmail.com>
2012-06-11 08:23:00 -0700

    .

--- src/export.sql
+++ src/export.sql
@@ -2,29 +2,21 @@
 set role to hardy;
 
 \o ../data/x_contrib_nodes.csv
-COPY (SELECT * FROM contrib_nodes) TO STDOUT WITH CSV HEADER
+COPY (SELECT * FROM contrib_nodes ORDER BY node_id) TO STDOUT WITH CSV HEADER
 ;
 
 \o ../data/x_contrib_paths.csv
-COPY (SELECT * FROM contrib_paths) TO STDOUT WITH CSV HEADER
+COPY (SELECT * FROM contrib_paths ORDER BY path_id) TO STDOUT WITH CSV HEADER
 ;
 
 \o ../data/x_contrib_flows.csv
 COPY (SELECT * FROM contrib_flows ORDER BY lang, yyyy, mm) TO STDOUT WITH CSV HEADER
 ;
 
+\o ../data/x_contrib_flows_yyyymm.csv
+COPY (SELECT * FROM contrib_flows_yyyymm ORDER BY lang, yyyy, mm) TO STDOUT WITH CSV HEADER
+;
+
 \o ../data/x_contrib_flows_yyyy.csv
 COPY (SELECT * FROM contrib_flows_yyyy ORDER BY lang, yyyy) TO STDOUT WITH CSV HEADER
 ;
-
--- \o ../data/x_contrib_summary_yyyymm.csv
--- COPY (SELECT * FROM contrib_summary_yyyymm) TO STDOUT WITH CSV HEADER
--- ;
--- 
--- \o ../data/x_contrib_summary_yyyy.csv
--- COPY (SELECT * FROM contrib_summary_yyyy) TO STDOUT WITH CSV HEADER
--- ;
--- 
--- \o ../data/x_contrib_summary.csv
--- COPY (SELECT * FROM contrib_summary) TO STDOUT WITH CSV HEADER
--- ;
--- src/model.sql
+++ src/model.sql
@@ -1,69 +1,46 @@
--- \c hardy_db
--- set role to hardy;
--- 
--- -- -- integrate all together into path summary table
--- -- -- each path is the link plus contributions that traveled on it
--- -- DROP TABLE IF EXISTS contrib_flows_yyyymm;
--- -- CREATE TABLE contrib_flows_yyyymm AS
--- -- SELECT        t.lang, 
--- --               t.yyyy, 
--- --               t.mm, 
--- --               p1.path_id,
--- --               SUM(p1.n_contrib)   AS n_contrib,
--- --               SUM(p1.n)           AS n_path,
--- --               COUNT(*)            AS n
--- -- FROM          contrib_by_month as t
--- -- JOIN          contrib_nodes_yyyymm t1 ON (t1.x = t.contrib_x AND t1.y = t.contrib_y AND t1.lang = t.lang)
--- -- JOIN          contrib_nodes_yyyymm t2 ON (t2.x = t.article_x AND t2.y = t.article_y AND t2.lang = t.lang)
--- -- JOIN          contrib_paths_yyyymm p1 ON (p1.lang = t.lang AND p1.src = t1.node_id AND p1.dst = t2.node_id)
--- -- GROUP BY      t.lang, t.yyyy, t.mm, p1.path_id
--- -- ;
--- -- 
--- -- ALTER TABLE contrib_flows_yyyymm ADD COLUMN flow_id serial NOT NULL PRIMARY KEY;
--- -- ALTER TABLE contrib_flows_yyyymm ADD CONSTRAINT ux_contrib_flows_yyyymm UNIQUE(lang, yyyy, mm, path_id);
--- -- 
--- 
--- -- integrate all together into summary table
--- DROP TABLE IF EXISTS contrib_summary_yyyymm;
--- CREATE TABLE contrib_summary_yyyymm AS
--- SELECT        lang, 
---               yyyy, 
---               mm, 
---               COUNT(*)                AS n,
---               SUM(n_contrib)          AS n_contrib,
---               COUNT(DISTINCT src)     AS n_src,
---               COUNT(DISTINCT dst)     AS n_dst,
---               AVG(distance_km)        AS d,
---               SUM(n_contrib * distance_km)/SUM(n_contrib) AS d_w
--- FROM          contrib_paths_yyyymm
--- GROUP BY      lang, yyyy, mm
--- ;
--- 
--- 
--- DROP TABLE IF EXISTS contrib_summary_yyyy;
--- CREATE TABLE contrib_summary_yyyy AS
--- SELECT        lang, 
---               yyyy, 
---               COUNT(*)                AS n,
---               SUM(n_contrib)          AS n_contrib,
---               COUNT(DISTINCT src)     AS n_src,
---               COUNT(DISTINCT dst)     AS n_dst,
---               AVG(distance_km)        AS d,
---               SUM(n_contrib * distance_km)/SUM(n_contrib) AS d_w
--- FROM          contrib_paths_yyyymm
--- GROUP BY      lang, yyyy
--- ;
--- 
--- 
--- DROP TABLE IF EXISTS contrib_summary_yyyy;
--- CREATE TABLE contrib_summary_yyyy AS
--- SELECT        lang, 
---               COUNT(*)                AS n,
---               SUM(n_contrib)          AS n_contrib,
---               COUNT(DISTINCT src)     AS n_src,
---               COUNT(DISTINCT dst)     AS n_dst,
---               AVG(distance_km)        AS d,
---               SUM(n_contrib * distance_km)/SUM(n_contrib) AS d_w
--- FROM          contrib_paths_yyyymm
--- GROUP BY      lang
--- ;
+\c hardy_db
+set role to hardy;
+
+DROP TABLE IF EXISTS contrib_flows;
+CREATE TABLE contrib_flows AS
+SELECT    lang, yyyy, mm, path_id, contrib_n
+FROM      contrib_by_month t
+JOIN      contrib_nodes t1 ON (t1.x = t.contrib_x AND t1.y = t.contrib_y)
+JOIN      contrib_nodes t2 ON (t2.x = t.article_x AND t2.y = t.article_y)
+JOIN      contrib_paths p1 ON (p1.node_id_src = t1.node_id AND p1.node_id_dst = t2.node_id)
+;
+
+ALTER TABLE contrib_flows ADD COLUMN flow_id serial NOT NULL PRIMARY KEY;
+
+DROP TABLE IF EXISTS contrib_flows_yyyymm;
+CREATE TABLE contrib_flows_yyyymm AS
+SELECT    lang, yyyy, mm, 
+          COUNT(*) AS n_path,
+          SUM(contrib_n * distance_km)/SUM(contrib_n) AS distance_mean_weighted,
+          AVG(distance_km) AS distance_mean,
+          (SUM(contrib_n * distance_km)/SUM(contrib_n))/AVG(distance_km) AS distance_ratio,
+          SUM(contrib_n) AS n_contrib
+FROM      contrib_flows t
+JOIN      contrib_paths p1 USING (path_id)
+GROUP BY  lang, yyyy, mm
+;
+
+ALTER TABLE contrib_flows_yyyymm ADD CONSTRAINT ux_contrib_flows_yyyymm UNIQUE(lang, yyyy, mm);
+
+
+DROP TABLE IF EXISTS contrib_flows_yyyy;
+CREATE TABLE contrib_flows_yyyy AS
+SELECT    lang, yyyy, 
+          COUNT(*) AS n_path,
+          SUM(contrib_n * distance_km)/SUM(contrib_n) AS distance_mean_weighted,
+          AVG(distance_km) AS distance_mean,
+          SUM(contrib_n) AS n_contrib
+FROM      contrib_flows t
+JOIN      contrib_paths p1 USING (path_id)
+GROUP BY  lang, yyyy
+;
+
+ALTER TABLE contrib_flows_yyyy ADD CONSTRAINT ux_contrib_flows_yyyy UNIQUE(lang, yyyy);
+
+
+
