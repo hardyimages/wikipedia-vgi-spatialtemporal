ee1adb8b399e79dda9c205044fe7d9d387eac656
Darren Hardy <hardy@nceas.ucsb.edu>
2012-06-14 07:06:35 -0700

    x

--- analysis/report.R
+++ analysis/report.R
@@ -1,3 +1,4 @@
+require(ohi)
 require(reshape)
 require(stringr)
 require(ggplot2)
@@ -10,20 +11,17 @@ d <- merge(d.flows, d.lang[,-1], by.x='lang', by.y='alpha2')
 
 d$name <- factor(d$name)
 summary(d)
-# p <- cast(yyyy + mm ~ name, data=d, value='distance_ratio')
-# data <- apply.bycol(p, function (x) { as.numeric(na.omit(x))})
-# 
-# plot(density(data$English), main='English', xlab='Distance ratio\n(weighted by contributions : unweighted)')
-# abline(v=mean(data$English, na.rm=T), lty=2, col='red')
-# 
-# 
-# boxplot((p[,3:ncol(p)]), pch=20,
-#   ylab='Distance ratio\n(weighted by contributions : unweighted)', 
-#   xlab='', las=3)
-# abline(h=1.0, lty=2, col='blue')
+p <- cast(yyyy + mm ~ name, data=d, value='distance_ratio')
+data <- apply.bycol(p, function (x) { as.numeric(na.omit(x))})
+ 
+pdf()
+plot(density(data$English), main='English', xlab='Distance ratio\n(weighted by contributions : unweighted)')
+abline(v=mean(data$English, na.rm=T), lty=2, col='red')
 
+summary(d)
 gp <- ggplot(d, aes(x=name, y=distance_ratio)) + geom_boxplot()
 gp <- gp + scale_y_continuous("Distance ratio\nweighted by contributions : unweighted")
 gp <- gp + scale_x_discrete("Language")
 gp <- gp + opts(axis.text.x=theme_text(angle=90, size=8, hjust=1))
 plot(gp)
+dev.off()
