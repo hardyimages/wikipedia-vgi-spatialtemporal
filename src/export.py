b7f1c8729711daf0261b908ed44f7c32bcca1d8c
Darren Hardy <hardy@nceas.ucsb.edu>
2012-06-13 22:42:10 -0700

    .

--- src/export.py
+++ src/export.py
@@ -1,19 +1,31 @@
 # this writes jflowview outputs
-#
-#   year month lang article_id article_x article_y contrib_x contrib_y contrib_n
-# 1 2004    10   no         65      11.4      48.8      11.8      60.7         1
-# 2 2004    10   no        140       5.6      58.7       5.7      58.7         4
-# 3 2004    10   no        173     -53.0     -10.6      10.4      59.3         1
-# 4 2004    10   no        185      14.4      67.3      11.8      60.7         1
-# 5 2004    10   no        185      14.4      67.3      13.7      66.9         2
-# 6 2004    10   no        260      10.2      59.7      10.8      59.9         1
+# #
+# hardy_db=> select * from v_contrib_by_month_k5 limit 10
+# ;
+#  lang | yyyy | mm | article_x | article_y | contrib_x | contrib_y | contrib_n 
+# ------+------+----+-----------+-----------+-----------+-----------+-----------
+#  ca   | 2006 |  1 |        -5 |        40 |         0 |        45 |         1
+#  ca   | 2006 |  1 |         0 |        40 |        -5 |        40 |        49
+#  ca   | 2006 |  1 |         0 |        40 |        -0 |        40 |         6
+#  ca   | 2006 |  1 |         5 |        50 |         5 |        50 |         1
+#  ca   | 2006 |  1 |        20 |        50 |        10 |        50 |         3
+#  ca   | 2006 |  1 |       125 |        10 |       -80 |        45 |         2
+#  ca   | 2006 |  1 |       125 |        40 |        -5 |        40 |         1
+#  cs   | 2006 |  1 |        -0 |        50 |        15 |        50 |         1
+#  cs   | 2006 |  1 |        15 |        50 |        15 |        50 |         5
+#  cs   | 2006 |  1 |        20 |        45 |        15 |        50 |         1
+# (10 rows)
 import csv
 import os
 import psycopg2
 import psycopg2.extras
 
-filter_contrib_n = 0
-cluster_k = 1
+filter_contrib_n = 1
+cluster_k = 10.0
+
+def cluster(x, y):
+    return (x, y)
+    # return (cluster_k*round(x/cluster_k), cluster_k*round(y/cluster_k))
 
 conn = psycopg2.connect("host=127.0.0.1 dbname=hardy_db user=hardy password=eEPerem65")
 print conn
@@ -22,19 +34,21 @@ cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 for lang in ["ca", "cs", "da", "de", "en", "eo", "es", "fi", "fr", "is", "it", "ja", "nl", "no", "pl", "pt", "ru", "sk", "sv", "tr", "zh"]:
     os.mkdir('data/%s' % (lang))
-
     for yr in range(2002,2008+1):
         for month in range(1,12+1):
             try:
                 print 'processing', lang, yr, month
                 nodes = {}
                 edges = []
-                cur.execute('''SELECT * FROM contrib_by_month WHERE lang = '%s' AND yyyy = %d AND mm = %d''' % (lang, yr, month))
+                sql = '''SELECT * FROM v_contrib_by_month_k5 WHERE lang = '%s' AND yyyy = %d AND mm = %d AND contrib_n >= %d''' % (lang, yr, month, filter_contrib_n)
+                cur.execute(sql)
                 for row in cur.fetchall():
+                    row['contrib_n'] = int(row['contrib_n'])
+                    if row['contrib_n'] < filter_contrib_n:
+                        continue
+                    
                     row['yyyy'] = int(row['yyyy'])
                     row['mm'] = int(row['mm'])
-                    row['contrib_n'] = int(row['contrib_n'])
-                    row['article_id'] = int(row['article_id'])
                     row['article_x'] = float(row['article_x'])
                     row['article_y'] = float(row['article_y'])
                     row['contrib_x'] = float(row['contrib_x'])
@@ -42,15 +56,14 @@ for lang in ["ca", "cs", "da", "de", "en", "eo", "es", "fi", "fr", "is", "it", "
                     # print lang, yr, month
                     # print row['lang'], row['yyyy'], row['mm']
                     if row['lang'] == lang and row['yyyy'] == yr and row['mm'] == month:
-                        xy_dst = (row['article_x'], row['article_y'])
-                        xy_src = (row['contrib_x'], row['contrib_y'])
+                        xy_dst = cluster(row['article_x'], row['article_y'])
+                        xy_src = cluster(row['contrib_x'], row['contrib_y'])
 
-                        if row['contrib_n'] >= filter_contrib_n:
-                            for xy in [xy_dst, xy_src]:
-                                if xy not in nodes:
-                                    nodes[xy] = 'N%d' % (len(nodes)+1)
+                        for xy in [xy_dst, xy_src]:
+                            if xy not in nodes:
+                                nodes[xy] = 'N%d' % (len(nodes)+1)
 
-                            edges.append([nodes[xy_src], nodes[xy_dst], row['contrib_n']])
+                        edges.append([nodes[xy_src], nodes[xy_dst], row['contrib_n']])
                     else:
                         break
 
