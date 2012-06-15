0052d3110aedca223c12a56275149f9f8c2cdb03
Darren Hardy <hardy@nceas.ucsb.edu>
2012-06-14 23:16:01 -0700

    x

--- src/import.sql
+++ src/import.sql
@@ -17,3 +17,20 @@ CREATE TABLE contrib_by_month (
 \copy contrib_by_month from '../data/x_contrib_by_month.csv' with csv header
 
 select count(*) from contrib_by_month;
+
+drop view if exists v_contrib_by_month_k1;
+create view v_contrib_by_month_k1 as
+  SELECT  yyyy, mm, lang, 
+          1.0*round(article_x/1.0) AS article_x,
+          1.0*round(article_y/1.0) AS article_y,
+          1.0*round(contrib_x/1.0) AS contrib_x,
+          1.0*round(contrib_y/1.0) AS contrib_y,
+          SUM(contrib_n) AS contrib_n,
+          COUNT(*) AS article_n
+  FROM    contrib_by_month
+  GROUP BY yyyy, mm, lang, 
+            1.0*round(article_x/1.0), 
+            1.0*round(article_y/1.0), 
+            1.0*round(contrib_x/1.0), 
+            1.0*round(contrib_y/1.0)
+;
