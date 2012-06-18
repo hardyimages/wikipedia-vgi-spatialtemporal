60162a98ca1a4db88347a998ea8d88e421f08f93
Hardy Images <hardyimages@gmail.com>
2012-06-18 08:13:50 -0700

    x

--- analysis/model.R
+++ analysis/model.R
@@ -7,7 +7,7 @@ lang.labels <- as.character(d.lang$name)
 names(lang.labels) <- str_trim(tolower(as.character(d.lang$alpha2)))
 
 if (FALSE) {
-  d.flows <- read.csv('data/x_contrib_flows_yyyymm.csv')
+  d.flows <- read.csv('../data/x_contrib_flows_yyyymm.csv')
   head(d.flows)
   results <- list()
   pdf(file='Rplots.pdf', width=7, height=7, onefile=T, pointsize=18)
@@ -18,33 +18,36 @@ if (FALSE) {
   dev.off()
 }
 
-
+# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1 
+pv.to.str <- function(x) {
+  if (x < 0.001) {
+    '***'
+  } else if (x < 0.01) {
+    '**'
+  } else if (x < 0.05) {
+    '*'
+  } else if (x < 0.1) {
+    '.'
+  } else {
+    ' '
+  }
+}
 ##
 #
-d.flows <- read.csv('data/x_contrib_flows_raw.csv')
+d.flows <- read.csv('../data/x_contrib_flows_raw.csv')
 d.flows$log10_d <- log10(d.flows$d + 1)
 r <- NULL
-for (l in unique(levels(d.flows$lang))) {
-  d.lang <- subset(d.flows, lang == l, select=-lang)
-  m <- lm(pop_dst ~ pop_src + log10_d, data=d.lang)
-  r <- rbind(r, data.frame(lang=lang.labels[[l]], yyyy=0,
-        n=nrow(d.lang),
-        r2=summary(m)$adj.r.squared, 
-        beta.pop_src=summary(m)$coefficients['pop_src','Estimate'], 
-        se.pop_src=summary(m)$coefficients['pop_src','Std. Error'], 
-        t.pop_src=summary(m)$coefficients['pop_src','t value'], 
-        pv.pop_src=summary(m)$coefficients['pop_src','Pr(>|t|)'], 
-        beta.d=summary(m)$coefficients['log10_d','Estimate'], 
-        se.d=summary(m)$coefficients['log10_d','Std. Error'], 
-        t.d=summary(m)$coefficients['log10_d','t value'], 
-        pv.d=summary(m)$coefficients['log10_d','Pr(>|t|)']))
-  
+rr <- NULL
+for (l in sort(lang.labels[unique(levels(d.flows$lang))])) {
+  ll <- names(lang.labels)[lang.labels == l]
+  d.lang <- subset(d.flows, lang == ll, select=-lang)
   for (yr in unique(d.lang$yyyy)) {
     d <- subset(d.lang, yyyy == yr, select=-yyyy)
-    if (length(unique(d$mm)) == 12) {
+    if (length(unique(d$mm)) == 12 && nrow(d) >= 100) {
       m <- lm(pop_dst ~ pop_src + log10_d, data=d)
-      r <- rbind(r, data.frame(lang=lang.labels[[l]], yyyy=yr,
+      rr <- rbind(rr, data.frame(lang=l, yyyy=yr,
             n=nrow(d),
+            n.n=sum(d$n),
             r2=summary(m)$adj.r.squared, 
             beta.pop_src=summary(m)$coefficients['pop_src','Estimate'], 
             se.pop_src=summary(m)$coefficients['pop_src','Std. Error'], 
@@ -56,12 +59,39 @@ for (l in unique(levels(d.flows$lang))) {
             pv.d=summary(m)$coefficients['log10_d','Pr(>|t|)']))
     }
   }
+  m <- lm(pop_dst ~ pop_src + log10_d, data=d.lang)
+  print(summary(m))
+  r <- rbind(r, data.frame(lang=l, yyyy=NA,
+        n=nrow(d.lang),
+        n.n=sum(d.lang$n),
+        r2=summary(m)$adj.r.squared, 
+        beta.pop_src=summary(m)$coefficients['pop_src','Estimate'], 
+        se.pop_src=summary(m)$coefficients['pop_src','Std. Error'], 
+        t.pop_src=summary(m)$coefficients['pop_src','t value'], 
+        pv.pop_src=summary(m)$coefficients['pop_src','Pr(>|t|)'], 
+        beta.d=summary(m)$coefficients['log10_d','Estimate'], 
+        se.d=summary(m)$coefficients['log10_d','Std. Error'], 
+        t.d=summary(m)$coefficients['log10_d','t value'], 
+        pv.d=summary(m)$coefficients['log10_d','Pr(>|t|)']))
+
 }
-rownames(r) <- NULL
-r <- r[order(r$lang, r$yyyy),]
-write.csv(r, '_xtable_flows.csv')
+rownames(r) <- r$lang
+rownames(rr) <- paste(rr$lang, rr$yyyy)
+write.csv(rbind(r, rr), '_xtable_flows.csv')
+
+r$nr <- r$n.n/r$n
+summary(r)
+r <- r[order(-r$n),]
+r <- r[,c(6,10,14,5)]
+summary(r)
 xtable(r, digits=3)
 
+rr$nr <- rr$n.n/rr$n
+rr <- rr[order(rr$lang, rr$yyyy),]
+rr <- rr[,c(6,10,14,5)]
+summary(rr)
+xtable(rr, digits=3)
+q()
 # lm(formula = pop_dst ~ pop_src + log(d + 1) + 0, data = z)
 # 
 # Residuals:
